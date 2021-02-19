19666d5ae548d70972d025f3e4cfa311a11c3f0b
Aimarall <aimaral21072004@gmail.com>
2021-02-19 16:39:13 +0600

    added fetch

--- scripts.js
+++ scripts.js
@@ -1,8 +1,37 @@
-let name = document.querySelector('#name');
-let message = document.querySelector('#message');
-let color = document.querySelector('#color');
-let button = document.querySelector('button');
+// let name = document.querySelector('#name');
+// let message = document.querySelector('#message');
+// let color = document.querySelector('#color');
+// let button = document.querySelector('button');
+//   let div = document.querySelector('#chat');
+// button.addEventListener('click', function () {
+  
+//     let spanName = document.createElement('span');
+//     let spanMeassge = document.querySelector('span');
+//     div.append(spanName);
+//     console.log(input.value);
 
-button.addEventListener('click', function(){
-    console.log(input.value);
+
+// });
+
+// document.body.append(div);
+
+
+fetch('https://it-academy-proect-default-rtdb.firebaseio.com/people.com')
+
+let btn1 = document.querySelector('#btn1')
+let name = document.querySelector('#name')
+let massege = document.querySelector('#massage')
+let color = document.querySelector('#color')
+let chat = document.querySelector('#chat')
+
+btn1.addEventListener("mousedown", function(){
+    let div = document.createElement('div')
+    let span1 = document.createElement('span')
+    let span2 = document.createElement('span')
+    div.append(span1)
+    div.append(span2)
+    chat.append(div)
+    span1.innerText = name.value + ": "
+    span2.innerText = massege.value
+    span1.style.color = color.value
 })
